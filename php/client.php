04c98269a7370a1b8f3ceea066a36c4b3ed5a258
Joyce Babu <joyce@ennexa.com>
2020-12-01 16:58:53 +0530

    style: fix coding style with php-cs-fixer

--- php/client.php
+++ php/client.php
@@ -1,13 +1,16 @@
 <?php
+
 namespace Prokerala\Api\Sample\Exception
 {
-    class ApiError extends \Exception {}
+    class ApiError extends \Exception
+    {
+    }
 
     class ValidationError extends \InvalidArgumentException
     {
         private $validationMessages;
 
-        public function __construct( $message, $code, $validationMessages)
+        public function __construct($message, $code, $validationMessages)
         {
             parent::__construct($message, $code, null);
             $this->validationMessages = $validationMessages;
@@ -19,15 +22,16 @@ namespace Prokerala\Api\Sample\Exception
         }
     }
 
-    class AuthenticationError extends \InvalidArgumentException {}
+    class AuthenticationError extends \InvalidArgumentException
+    {
+    }
 
-    class ServerError extends \RuntimeException {}
+    class ServerError extends \RuntimeException
+    {
+    }
 }
 
 namespace Prokerala\Api\Sample {
-
-    use Throwable;
-
     class ApiClient
     {
         const BASE_URL = 'https://api.prokerala.com/';
@@ -40,81 +44,6 @@ namespace Prokerala\Api\Sample {
          * @var string
          */
         private $clientSecret;
-
-        private function saveToken($token)
-        {
-            // TODO: Cache the token until it expires
-        }
-
-        private function getTokenFromCache()
-        {
-            // TODO: Return cached token, if exists
-        }
-
-        private function fetchNewToken()
-        {
-            $data = [
-                'grant_type' => 'client_credentials',
-                'client_id' => $this->clientId,
-                'client_secret' => $this->clientSecret,
-            ];
-
-            $uri = self::BASE_URL . 'token';
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $uri);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-            $response = curl_exec($ch);
-            $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-            curl_close($ch);
-
-
-            $token = $this->parseResponse($response, $responseCode, $contentType);
-            $this->saveToken($token);
-
-            return $token->access_token;
-        }
-
-        /**
-         * Parse API Response.
-         * @param string $response     Response body.
-         * @param int    $responseCode HTTP Status code.
-         * @param string $contentType  Response content type.
-         * @return string|\stdClass
-         */
-        private function parseResponse($response, $responseCode, $contentType)
-        {
-            $res = $response;
-
-            if ('application/json' === strtok($contentType, ';')) {
-                $res = json_decode($res);
-            }
-
-            if ($responseCode === 200) {
-                return $res;
-            }
-
-            if ($res->status !== "error") {
-                throw new Exception\ApiError( "HTTP request failed" );
-            }
-
-            $errors = $res->errors;
-
-            if ($responseCode === 400) {
-                throw new Exception\ValidationError('Validation failed', 400, $errors);
-            }
-
-            if ($responseCode === 401) {
-                throw new Exception\AuthenticationError($errors[0]->detail, 403);
-            }
-
-            if ($responseCode >= 500) {
-                throw new Exception\ServerError($errors[0]->detail, $responseCode);
-            }
-
-		    throw new Exception\ApiError('Unexpected error');
-        }
 
         public function __construct($clientId, $clientSecret)
         {
@@ -145,6 +74,82 @@ namespace Prokerala\Api\Sample {
             curl_close($ch); // Close the connection
 
             return $this->parseResponse($response, $responseCode, $contentType);
+        }
+
+        private function saveToken($token)
+        {
+            // TODO: Cache the token until it expires
+        }
+
+        private function getTokenFromCache()
+        {
+            // TODO: Return cached token, if exists
+        }
+
+        private function fetchNewToken()
+        {
+            $data = [
+                'grant_type' => 'client_credentials',
+                'client_id' => $this->clientId,
+                'client_secret' => $this->clientSecret,
+            ];
+
+            $uri = self::BASE_URL . 'token';
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $uri);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+            $response = curl_exec($ch);
+            $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+            curl_close($ch);
+
+            $token = $this->parseResponse($response, $responseCode, $contentType);
+            $this->saveToken($token);
+
+            return $token->access_token;
+        }
+
+        /**
+         * Parse API Response.
+         *
+         * @param string $response     response body
+         * @param int    $responseCode HTTP Status code
+         * @param string $contentType  response content type
+         *
+         * @return \stdClass|string
+         */
+        private function parseResponse($response, $responseCode, $contentType)
+        {
+            $res = $response;
+
+            if ('application/json' === strtok($contentType, ';')) {
+                $res = json_decode($res);
+            }
+
+            if (200 === $responseCode) {
+                return $res;
+            }
+
+            if ('error' !== $res->status) {
+                throw new Exception\ApiError('HTTP request failed');
+            }
+
+            $errors = $res->errors;
+
+            if (400 === $responseCode) {
+                throw new Exception\ValidationError('Validation failed', 400, $errors);
+            }
+
+            if (401 === $responseCode) {
+                throw new Exception\AuthenticationError($errors[0]->detail, 403);
+            }
+
+            if ($responseCode >= 500) {
+                throw new Exception\ServerError($errors[0]->detail, $responseCode);
+            }
+
+            throw new Exception\ApiError('Unexpected error');
         }
     }
 }
