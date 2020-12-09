54da764d76aa39d02bcea91935a31c58dc8e72ac
Joyce Babu <joyce@ennexa.com>
2020-12-09 16:28:07 +0530

    fix: handle exception and print error message

--- python/test.py
+++ python/test.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
-from client import ApiClient
+from client import ApiClient, ValidationError, AuthenticationError
 import json
 
 def run():
-	client = ApiClient('YOUR_CLIENT_ID', 'YOUR_CLIENT_SECRET')
-	result = client.get('v2/astrology/kundli/advanced', {
-		'ayanamsa': 1,
-		'coordinates': '23.1765,75.7885',
-		'datetime': '2020-10-19T12:31:14+00:00'
-	})
-	print json.dumps(result, indent=4)
+	try:
+		client = ApiClient('YOUR_CLIENT_ID', 'YOUR_CLIENT_SECRET')
+		result = client.get('v2/astrology/kundli/advanced', {
+			'ayanamsa': 1,
+			'coordinates': '23.1765,75.7885',
+			'datetime': '2020-10-19T12:31:14+00:00',
+		})
+		print json.dumps(result, indent=4)
+	except ValidationError as e:
+		for msg in e.getValidationMessages():
+			print(msg['detail'])
+	except AuthenticationError as e:
+		print (e.message)
 
 if __name__ == '__main__':
 	run()
